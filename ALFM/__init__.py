814dd402161fc035fdf8b297d2ef8754e28a0150
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-04-27 14:00:57 -0700

    added model enum to the top level of the package

--- ALFM/__init__.py
+++ ALFM/__init__.py
@@ -1,6 +1,10 @@
 """Package level aliased imports."""
 
 from ALFM.src.datasets.registry import DatasetType
+from ALFM.src.models.registry import ModelType
 
 
-__all__ = ["DatasetType"]
+__all__ = [
+    "DatasetType",
+    "ModelType",
+]
