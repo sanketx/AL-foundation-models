684b50d7a8eb34322d648496ca86a273be801e80
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-04-28 05:48:01 -0700

    modified wrapper to also return class labels

--- ALFM/src/models/backbone_wrapper.py
+++ ALFM/src/models/backbone_wrapper.py
@@ -1,11 +1,12 @@
 """Wrapper module for a PyTorch model for use with PyTorch Lightning."""
 
+from typing import Tuple
 from typing import cast
 
 import numpy as np
-import numpy.typing as npt
 import pytorch_lightning as pl
 import torch
+from numpy.typing import NDArray
 from torch import nn
 
 
@@ -27,7 +28,7 @@ class BackboneWrapper(pl.LightningModule):
 
     def predict_step(
         self, batch: torch.Tensor, batch_idx: int, dataloader_idx: int = 0
-    ) -> npt.NDArray[np.float32 | np.float16]:
+    ) -> Tuple[NDArray[np.float32 | np.float16], NDArray[np.int64]]:
         """Prediction step for a batch of data.
 
         Args:
@@ -36,8 +37,12 @@ class BackboneWrapper(pl.LightningModule):
             dataloader_idx (int, optional): Index of the current dataloader.
 
         Returns:
-            npt.NDArray[np.float32 | np.float16]: Model predictions for the input batch.
+            Tuple[NDArray[np.float32 | np.float16], NDArray[np.int64]]: features
+                and labels for the input batch.
         """
         x, y = batch
         predictions = self.model(x).cpu().numpy()
-        return cast(npt.NDArray[np.float32 | np.float16], predictions)
+        labels = y.cpu().numpy().reshape(-1, 1).astype(np.int64)
+        features = cast(NDArray[np.float32 | np.float16], predictions)
+
+        return features, labels
