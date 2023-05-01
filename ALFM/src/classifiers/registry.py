e1abc85fc10e5dd923d3f182da390cb980cf05cb
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-01 14:08:41 -0700

    added residual adapter to the registry

--- ALFM/src/classifiers/registry.py
+++ ALFM/src/classifiers/registry.py
@@ -3,9 +3,11 @@
 from enum import Enum
 
 from ALFM.src.classifiers.linear_classifier import LinearClassifier
+from ALFM.src.classifiers.residual_adapter import ResidualAdapter
 
 
 class ClassifierType(Enum):
     """Enum of supported Classifiers."""
 
     linear_classifier = LinearClassifier
+    residual_adapter = ResidualAdapter
