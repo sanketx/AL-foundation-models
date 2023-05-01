771ac5a0a5716e5a91cc30b409d6eff69cf7855b
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-01 04:45:07 -0700

    added util functions for logging results to console

---
+++ ALFM/src/run/utils.py
@@ -0,0 +1,30 @@
+"""Utilities for Active Learning experiments."""
+
+from typing import Dict
+
+import numpy as np
+from ALFM.src.run.logging import logger
+from numpy.typing import NDArray
+
+
+def print_composition(
+    features: NDArray[np.float32], labels: NDArray[np.int64], mask: NDArray[np.bool_]
+) -> None:
+    total_samples = len(features)
+    num_samples = len(features[mask])
+    num_classes = len(np.unique(labels))
+    seen_classes = len(np.unique(labels[mask]))
+    num_features = features.shape[1]
+
+    logger.info(
+        f"Training on {num_samples}/{total_samples} samples with dim: "
+        + f"{num_features}, seen {seen_classes}/{num_classes} classes"
+    )
+
+
+def print_scores(scores: Dict[str, float], i: int, num_iter: int, budget: int) -> None:
+    logger.info(
+        f"[{i}/{num_iter}] Budget: {budget} "
+        + f"| Acc: {scores['TEST_MulticlassAccuracy']:.3f}"
+        + f" | AUROC: {scores['TEST_MulticlassAUROC']:.3f}\n"
+    )
