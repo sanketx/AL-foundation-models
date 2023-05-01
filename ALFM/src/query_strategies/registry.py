247fb3860bada51786dc1155945554b2b90bc634
Josiah Aklilu <josaklil@stanford.edu>
2023-05-01 15:35:24 -0700

    added new queries to registry

--- ALFM/src/query_strategies/registry.py
+++ ALFM/src/query_strategies/registry.py
@@ -3,9 +3,17 @@
 from enum import Enum
 
 from ALFM.src.query_strategies.random import Random
+from ALFM.src.query_strategies.uncertainty import Uncertainty
+from ALFM.src.query_strategies.entropy import Entropy
+from ALFM.src.query_strategies.margins import Margins
+from ALFM.src.query_strategies.coreset import Coreset
 
 
 class QueryType(Enum):
     """Enum of supported Active Learning query methods."""
 
     random = Random
+    uncertainty = Uncertainty
+    entropy = Entropy
+    margins = Margins
+    coreset = Coreset
