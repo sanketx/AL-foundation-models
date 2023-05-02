44ef661a814a7917b5374ddad3ffd15e8c0818a1
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-01 18:25:42 -0700

    refactored residual mlp and removed forward function

--- ALFM/src/classifiers/residual_adapter.py
+++ ALFM/src/classifiers/residual_adapter.py
@@ -1,45 +1,38 @@
 """Residual Adapter model."""
 
-from typing import List
+from typing import Any
 
 import torch
-import torch.nn.functional as F
 from ALFM.src.classifiers.base_classifier import BaseClassifier
-from torch import Tensor
 from torch import nn
-from torchmetrics import Metric
 
 
-class ResidualAdapter(BaseClassifier):
-    def __init__(
-        self,
-        input_dim: int,
-        num_classes: int,
-        dropout_p: float,
-        lr: float,
-        weight_decay: float,
-        metrics: List[Metric],
-        scale: int,
-        init_alpha: float,
-    ) -> None:
-        super().__init__(input_dim, num_classes, dropout_p, lr, weight_decay, metrics)
-        self.batch_norm = nn.BatchNorm1d(input_dim, affine=False)
+class ResidualMLP(nn.Module):
+    def __init__(self, input_dim: int, scale: int, init_alpha: float) -> None:
+        super().__init__()
         self.alpha = nn.Parameter(torch.tensor(init_alpha, dtype=torch.float32))
 
-        self.stem = nn.Sequential(  # bottleneck MLP
+        self.mlp = nn.Sequential(  # bottleneck MLP
             nn.Linear(input_dim, input_dim // scale),
             nn.GELU(),
             nn.Linear(input_dim // scale, input_dim),
             nn.GELU(),
         )
 
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.dropout(x)
-
-        x_adapt = self.stem(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x_adapt = self.mlp(x)
         alpha = torch.sigmoid(self.alpha)
         x = alpha * x_adapt + (1 - alpha) * x
-
-        x = self.batch_norm(x)
-        x = self.linear(x)
         return x
+
+
+class ResidualAdapter(BaseClassifier):
+    def __init__(
+        self, input_dim: int, scale: int, init_alpha: float, **params: Any
+    ) -> None:
+        super().__init__(input_dim, **params)
+        self.feature_extractor = nn.Sequential(
+            self.dropout,
+            ResidualMLP(input_dim, scale, init_alpha),
+            nn.BatchNorm1d(input_dim, affine=False),
+        )
