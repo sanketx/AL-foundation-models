6c18a90fbb14d1b07fc8964272c0f19ec8051043
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-01 23:22:10 -0700

    created new wrapper module to standardize formats

---
+++ ALFM/src/datasets/dataset_wrappers.py
@@ -0,0 +1,22 @@
+"""Wrapper functions to standardize vision datasets."""
+
+
+from typing import Optional
+
+from torchvision import transforms
+from torchvision.datasets import Food101
+from torchvision.datasets import VisionDataset
+
+
+class Food101Wrapper:
+    @staticmethod  # don't even ask
+    def __call__(
+        root: str,
+        train: bool,
+        transform: Optional[transforms.Compose] = None,
+        download: bool = False,
+    ) -> VisionDataset:
+        print(download)
+
+        split = "train" if train else "test"
+        return Food101(root, split, transform, download=download)
