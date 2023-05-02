024cea2312131ed718fd2a9e0865b9a1155c0015
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-01 18:20:44 -0700

    refactored code to make the implementation simpler

--- ALFM/src/classifiers/linear_classifier.py
+++ ALFM/src/classifiers/linear_classifier.py
@@ -1,28 +1,14 @@
 """Linear classification model."""
 
-from typing import List
+from typing import Any
 
 from ALFM.src.classifiers.base_classifier import BaseClassifier
-from torch import Tensor
 from torch import nn
-from torchmetrics import Metric
 
 
 class LinearClassifier(BaseClassifier):
-    def __init__(
-        self,
-        input_dim: int,
-        num_classes: int,
-        dropout_p: float,
-        lr: float,
-        weight_decay: float,
-        metrics: List[Metric],
-    ) -> None:
-        super().__init__(input_dim, num_classes, dropout_p, lr, weight_decay, metrics)
-        self.batch_norm = nn.BatchNorm1d(input_dim, affine=False)
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.dropout(x)
-        x = self.batch_norm(x)
-        x = self.linear(x)
-        return x
+    def __init__(self, input_dim: int, **params: Any) -> None:
+        super().__init__(input_dim, **params)
+        self.feature_extractor = nn.Sequential(
+            self.dropout, nn.BatchNorm1d(input_dim, affine=False)
+        )
