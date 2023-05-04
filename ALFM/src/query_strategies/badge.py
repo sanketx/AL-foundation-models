cb2ef417bd03c526f7373f091556d3d1a2acf7cf
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-03 22:56:26 -0700

    optimized badge query with pytorch backend

--- ALFM/src/query_strategies/badge.py
+++ ALFM/src/query_strategies/badge.py
@@ -3,6 +3,7 @@
 from typing import Any
 
 import numpy as np
+import torch
 from ALFM.src.query_strategies.base_query import BaseQuery
 from numpy.typing import NDArray
 from rich.progress import track
@@ -17,22 +18,22 @@ class BADGE(BaseQuery):
 
     def _pairwise_distances(
         self,
-        Z1: NDArray[np.float32],
-        P1: NDArray[np.float32],
-        Z2: NDArray[np.float32],
-        P2: NDArray[np.float32],
-    ) -> NDArray[np.float32]:
-        term1 = np.sum(Z1**2, axis=1)[:, None] * np.sum(P1**2, axis=1)[:, None]
-        term4 = np.sum(Z2**2, axis=1)[None, :] * np.sum(P2**2, axis=1)[None, :]
+        Z1: torch.Tensor,
+        P1: torch.Tensor,
+        Z2: torch.Tensor,
+        P2: torch.Tensor,
+    ) -> torch.Tensor:
+        term1 = torch.sum(Z1**2, dim=1)[:, None] * torch.sum(P1**2, dim=1)[:, None]
+        term4 = torch.sum(Z2**2, dim=1)[None, :] * torch.sum(P2**2, dim=1)[None, :]
         term2 = (Z1 @ Z2.T) * (P1 @ P2.T)
 
         frob_norm_squared_matrix = term1 - 2 * term2 + term4
-        return frob_norm_squared_matrix  # type: ignore[no-any-return]
+        return frob_norm_squared_matrix
 
     def _select_samples(
         self,
-        vectors: NDArray[np.float32],
-        delta: NDArray[np.float32],
+        vectors: torch.Tensor,
+        delta: torch.Tensor,
         num_samples: int,
     ) -> NDArray[np.int64]:
         centroids = []
@@ -45,19 +46,23 @@ class BADGE(BaseQuery):
         # Compute the squared distance from all points to the centroid
         centroid_vector = (vectors[idx][None, :], delta[idx][None, :])
         sq_dist = self._pairwise_distances(*centroid_vector, vectors, delta).ravel()
-        sq_dist[idx] = 0  # avoid numerical errors
+        sq_dist[sq_dist < 0] = 0  # avoid numerical errors
 
         # Choose the remaining centroids
         for _ in track(range(1, num_samples), description="[green]Badge query"):
-            probabilities = sq_dist / np.sum(sq_dist)
-            idx = np.random.choice(n, p=probabilities)
+            probabilities = sq_dist / torch.sum(sq_dist)
+
+            if torch.nonzero(probabilities < 0).any():
+                print(idx, torch.nonzero(probabilities < 0))
+
+            idx = torch.multinomial(probabilities, 1).item()  # type: ignore[assignment]
             centroids.append(idx)
 
             # update the squared distances
             centroid_vector = (vectors[idx][None, :], delta[idx][None, :])
             n_dist = self._pairwise_distances(*centroid_vector, vectors, delta).ravel()
-            sq_dist = np.minimum(sq_dist, n_dist)
-            sq_dist[idx] = 0  # avoid numerical errors
+            sq_dist = torch.minimum(sq_dist, n_dist)
+            sq_dist[sq_dist < 0] = 0  # avoid numerical errors
 
         return np.array(centroids)
 
@@ -71,9 +76,11 @@ class BADGE(BaseQuery):
             NDArray[np.bool_]: A boolean mask for the selected samples.
         """
         unlabeled_features = self.features[~self.labeled_pool]
-        probs, vectors = self.model.get_probs_and_embedding(unlabeled_features)
+        probs, embedding = self.model.get_probs_and_embedding(unlabeled_features)
         labels = np.argmax(probs, keepdims=True)
-        delta = probs - labels
+
+        delta = torch.from_numpy(probs - labels)
+        vectors = torch.from_numpy(embedding)
 
         if num_samples > len(unlabeled_features):
             raise ValueError(
