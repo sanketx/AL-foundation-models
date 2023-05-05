94e21def57926452ec979996002c43fb5969f58a
Josiah Aklilu <josaklil@stanford.edu>
2023-05-04 23:13:13 -0700

    finished BALD query

--- ALFM/src/query_strategies/bald.py
+++ ALFM/src/query_strategies/bald.py
@@ -14,9 +14,35 @@ class BALD(BaseQuery):
     (https://arxiv.org/abs/1703.02910).
     """
 
-    def __init__(self, **params: Any) -> None:
+    def __init__(self, M: int, **params: Any) -> None:
         """Call the superclass constructor."""
         super().__init__(**params)
+        self.M = M
+
+    def _get_mc_samples(self, features: NDArray[np.float32]) -> NDArray[np.float32]:
+        """Get MC samples from the model.
+
+        Returns:
+            NDArray[np.float32]: MC samples from the model.
+        """
+        samples = np.stack(
+            [
+                self.model.get_probs(features, dropout=True)
+                for _ in range(self.M)
+            ]
+        )
+        return samples
+
+    def _shannon_entropy(self, probs: NDArray[np.float32]) -> NDArray[np.float32]:
+        """Calculate the Shannon entropy of the given softmax probabilities.
+
+        Args:
+            probs (NDArray[np.float32]): The probabilities.
+
+        Returns:
+            NDArray[np.float32]: The Shannon entropy.
+        """
+        return -np.sum(probs * np.log(probs + 1e-10), axis=-1)
 
     def query(self, num_samples: int) -> NDArray[np.bool_]:
         """Select a new set of datapoints to be labeled.
@@ -27,4 +53,20 @@ class BALD(BaseQuery):
         Returns:
             NDArray[np.bool_]: A boolean mask for the selected samples.
         """
-        pass
+        mask = np.zeros(len(self.features), dtype=bool)
+        unlabeled_indices = np.flatnonzero(~self.labeled_pool)
+
+        if num_samples > len(unlabeled_indices):
+            raise ValueError(
+                f"num_samples ({num_samples}) is greater than unlabeled pool size ({len(unlabeled_indices)})"
+            )
+
+        mc_samples = self._get_mc_samples(self.features[unlabeled_indices])
+
+        H = self._shannon_entropy(np.mean(mc_samples, axis=0))
+        E = np.mean(self._shannon_entropy(mc_samples), axis=0)
+        mutual_information = H - E
+
+        indices = np.argsort(mutual_information)[-num_samples:]
+        mask[unlabeled_indices[indices]] = True
+        return mask
