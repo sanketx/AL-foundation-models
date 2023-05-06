29130761baaea4ecb1db0d6df231d899d3fb116a
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-06 13:08:00 -0700

    added multi-gpu inference using shared memory

--- ALFM/src/run/feature_extraction.py
+++ ALFM/src/run/feature_extraction.py
@@ -2,14 +2,20 @@
 
 import logging
 import os
+from multiprocessing.shared_memory import SharedMemory
 from typing import Any
 from typing import Callable
+from typing import Optional
+from typing import Sequence
+from typing import Tuple
 
 import h5py
+import hydra
 import numpy as np
 import pytorch_lightning as pl
 import torch
 from numpy.typing import NDArray
+from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 
 from ALFM.src.datasets.factory import create_dataset
@@ -21,6 +27,67 @@ from ALFM.src.models.registry import ModelType
 
 logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 torch.set_float32_matmul_precision("medium")  # type: ignore[no-untyped-call]
+
+
+class SharedMemoryWriter(pl.callbacks.BasePredictionWriter):
+    """Writes multi-GPU predictions to shared memory."""
+
+    def __init__(self, num_samples: int, num_classes: int, num_features: int) -> None:
+        """Create a new SharedMemoryWriter callback.
+
+        Args:
+            num_samples (int): number of samples in the dataset.
+            num_classes (int): number of classes in the dataset.
+            num_features (int): number of features in the dataset.
+        """
+        super().__init__(write_interval="batch")
+        self.num_samples = num_samples
+        self.num_classes = num_classes
+        self.num_features = num_features
+
+        self.feature_shm = SharedMemory(
+            create=True, size=4 * num_samples * num_features
+        )
+        self.label_shm = SharedMemory(create=True, size=8 * num_samples)
+
+        self.features: NDArray[np.float32] = np.ndarray(
+            (num_samples, num_features),
+            dtype=np.float32,
+            buffer=self.feature_shm.buf,
+        )
+        self.labels: NDArray[np.int64] = np.ndarray(
+            (num_samples, 1),
+            dtype=np.int64,
+            buffer=self.label_shm.buf,
+        )
+
+        self.features[:] = -1
+        self.labels[:] = -1
+
+    def write_on_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        predictions: Any,
+        batch_indices: Optional[Sequence[Any]],
+        batch: Any,
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        """Write predictions from each process to shared memory."""
+        self.features[batch_indices] = predictions[0]
+        self.labels[batch_indices] = predictions[1]
+
+    def get_predictions(self) -> Tuple[NDArray[np.float32], NDArray[np.int64]]:
+        """Return detached copies of prediction vectors."""
+        return self.features, self.labels
+
+    def close(self) -> None:
+        """Release shared memory."""
+        self.feature_shm.close()
+        self.feature_shm.unlink()
+        self.label_shm.close()
+        self.label_shm.unlink()
 
 
 def check_existing_features(vector_file: str, split: str) -> bool:
@@ -41,7 +108,7 @@ def check_existing_features(vector_file: str, split: str) -> bool:
 
 
 def save_vectors(
-    features: NDArray[np.float32 | np.float16],
+    features: NDArray[np.float32],
     labels: NDArray[np.int64],
     vector_file: str,
     split: str,
@@ -49,7 +116,7 @@ def save_vectors(
     """Save the extracted features to an HDF file.
 
     Args:
-        features (NDArray[np.float32 | np.float16]): Extracted features as a NumPy array.
+        features (NDArray[np.float32]): Extracted features as a NumPy array.
         labels (NDArray[np.int64]): Image labels as a NumPy array.
         vector_file (str): Path to the HDF file to save the features.
         split (str): Split name, either 'train' or 'test'.
@@ -60,27 +127,30 @@ def save_vectors(
 
 
 def extract_features(
-    dataset_type: DatasetType,
+    dataset_cfg: DictConfig,
     train: bool,
-    model_type: ModelType,
+    model_cfg: DictConfig,
     dataset_dir: str,
     model_dir: str,
     feature_dir: str,
     dataloader: Callable[..., DataLoader[Any]],
-    trainer: pl.Trainer,
+    trainer_cfg: DictConfig,
 ) -> None:
     """Extract features from the dataset using the specified pretrained model and save them to disk.
 
     Args:
-        dataset_type (DatasetType): Enum representing the type of dataset to use.
+        dataset_cfg (DictConfig): Config representing the dataset to use.
         train (bool): True if extracting features for the training set, False for the test set.
-        model_type (ModelType): Enum representing the type of pretrained model to use.
+        model_cfg (DictConfig): Config representing the pretrained model to use.
         dataset_dir (str): Path to the directory containing the dataset.
         model_dir (str): Path to the directory containing the model cache.
         feature_dir (str): Path to the directory where extracted features will be saved.
         dataloader (Callable[..., DataLoader[Any]]): Callable to create DataLoader for the dataset.
-        trainer (pl.Trainer): PyTorch Lightning Trainer for feature extraction.
+        trainer_cfg (pl.Trainer): PyTorch Lightning Trainer config for feature extraction.
     """
+    dataset_type = DatasetType[dataset_cfg.name]
+    model_type = ModelType[model_cfg.name]
+
     split = "train" if train else "test"
     dst_dir = os.path.join(feature_dir, f"{dataset_type.name}")
     vector_file = os.path.join(dst_dir, f"{model_type.name}.hdf")
@@ -99,10 +169,14 @@ def extract_features(
     )
 
     model = BackboneWrapper(model)
-    preds = trainer.predict(model, dataloader(dataset))
+    shm_writer = SharedMemoryWriter(
+        len(dataset), dataset_cfg.num_classes, model_cfg.num_features
+    )
 
-    feature_list, label_list = zip(*preds)  # type: ignore[misc]
-    features = np.concatenate(feature_list)
-    labels = np.concatenate(label_list)
+    prog_bar = pl.callbacks.RichProgressBar()
+    trainer = hydra.utils.instantiate(trainer_cfg, callbacks=[shm_writer, prog_bar])
+    trainer.predict(model, dataloader(dataset), return_predictions=False)
 
+    features, labels = shm_writer.get_predictions()
     save_vectors(features, labels, vector_file, split)
+    shm_writer.close()
