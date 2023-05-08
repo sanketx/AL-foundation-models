added numerical stability fixes and gpu optimizations

ALFM/src/clustering/kmeans.py
@@ -27,7 +27,7 @@ def kmeans_plus_plus_init(features: NDArray[np.float32], k: int) -> NDArray[np.i
     # pairwise_distances in FAISS returns the squared L2 distance
     centroid_vector = vectors[idx].view(1, -1)
     sq_dist = faiss_pd(vectors, centroid_vector).ravel()
-    sq_dist[centroids] = 0  # avoid numerical errors
+    sq_dist[sq_dist < 0] = 0  # avoid numerical errors
 
     # Choose the remaining centroids
     for _ in track(range(1, k), description="[green]K-Means++ init"):
@@ -38,11 +38,7 @@ def kmeans_plus_plus_init(features: NDArray[np.float32], k: int) -> NDArray[np.i
         # update the squared distances
         centroid_vector = vectors[idx].view(1, -1)
         new_dist = faiss_pd(vectors, centroid_vector).ravel()
-        new_dist[centroids] = 0  # avoid numerical errors
-
-        if new_dist.min() < 0:
-            print(new_dist.min(), new_dist.argmin())
-            print(centroids)
+        new_dist[new_dist < 0] = 0  # avoid numerical errors
 
         # update the minimum squared distance
         sq_dist = torch.minimum(sq_dist, new_dist)

ALFM/src/query_strategies/badge.py
@@ -48,7 +48,7 @@ class BADGE(BaseQuery):
         # Compute the squared distance from all points to the centroid
         centroid_vector = (vectors[idx][None, :], delta[idx][None, :])
         sq_dist = self._pairwise_distances(*centroid_vector, vectors, delta).ravel()
-        sq_dist[centroids] = 0  # avoid numerical errors
+        sq_dist[sq_dist < 0] = 0  # avoid numerical errors
 
         # Choose the remaining centroids
         for _ in track(range(1, num_samples), description="[green]Badge query"):
@@ -59,7 +59,7 @@ class BADGE(BaseQuery):
             # compute the new squared distances
             centroid_vector = (vectors[idx][None, :], delta[idx][None, :])
             n_dist = self._pairwise_distances(*centroid_vector, vectors, delta).ravel()
-            n_dist[centroids] = 0  # avoid numerical errors
+            n_dist[n_dist < 0] = 0  # avoid numerical errors
 
             # update the minimum squared distance
             sq_dist = torch.minimum(sq_dist, n_dist)

ALFM/src/query_strategies/coreset.py
@@ -7,7 +7,6 @@ import torch
 from numpy.typing import NDArray
 from rich.progress import track
 
-from ALFM.src.clustering.kmeans import faiss_pd
 from ALFM.src.query_strategies.base_query import BaseQuery
 
 
@@ -38,26 +37,26 @@ class Coreset(BaseQuery):
             NDArray[np.bool_]: A boolean mask for the selected samples.
         """
         vectors = self.model.get_embedding(self.features)
-        labeled_vectors = vectors[self.labeled_pool]
-        unlabeled_vectors = vectors[~self.labeled_pool]
+        labeled_vectors = vectors[self.labeled_pool].cuda().unsqueeze(0)
+        unlabeled_vectors = vectors[~self.labeled_pool].cuda().unsqueeze(0)
 
-        if num_samples > len(unlabeled_vectors):
+        if num_samples > unlabeled_vectors.shape[1]:
             raise ValueError(
-                f"num_samples ({num_samples}) is greater than unlabeled pool size ({len(unlabeled_vectors)})"
+                f"num_samples ({num_samples}) is greater than unlabeled pool size ({unlabeled_vectors.shape[1]})"
             )
 
         # pairwise_distances in FAISS returns the squared L2 distance
-        p_dist = faiss_pd(labeled_vectors, unlabeled_vectors)
+        p_dist = torch.cdist(labeled_vectors, unlabeled_vectors).squeeze()
         min_dist = p_dist.min(dim=0)[0]  # distance of UL points to the nearest center
 
         new_batch = []
 
         for _ in track(range(num_samples), description="[green]Core-Set query"):
             next_center = min_dist.argmax()
-            new_batch.append(next_center)
+            new_batch.append(next_center.item())
 
-            new_dist = faiss_pd(
-                unlabeled_vectors[next_center].view(1, -1),
+            new_dist = torch.cdist(
+                unlabeled_vectors[0, next_center].view(1, 1, -1),
                 unlabeled_vectors,
             )
             min_dist = torch.minimum(min_dist, new_dist.ravel())