60b567cc892129eec903c96772abcf8c8dfc3756
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-08 19:55:19 -0700

    added typiclust init to registry

--- ALFM/src/init_strategies/registry.py
+++ ALFM/src/init_strategies/registry.py
@@ -3,9 +3,11 @@
 from enum import Enum
 
 from ALFM.src.init_strategies.random_init import RandomInit
+from ALFM.src.init_strategies.typiclust_init import TypiclustInit
 
 
 class InitType(Enum):
     """Enum of supported inital query methods."""
 
     random_init = RandomInit
+    typiclust_init = TypiclustInit
