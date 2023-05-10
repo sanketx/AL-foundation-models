c93fcbc3c6c5255ca9867c44d66d81d5b0f40b3c
Josiah Aklilu <josaklil@stanford.edu>
2023-05-10 02:36:09 -0700

    added custom image folder for loading official splits

--- ALFM/src/datasets/utils.py
+++ ALFM/src/datasets/utils.py
@@ -16,7 +16,9 @@ from torchvision.datasets.folder import default_loader
 from torchvision.datasets.utils import download_and_extract_archive
 from torchvision.datasets.utils import download_url
 from torchvision.datasets.utils import verify_str_arg
-from torchvision.datasets.vision import VisionDataset
+
+from torchvision.datasets import DatasetFolder
+from torchvision.datasets import VisionDataset
 
 
 class INaturalist2021(VisionDataset):
@@ -296,3 +298,30 @@ class Cub2011(VisionDataset):
             img = self.transform(img)
 
         return img, target
+
+    
+class CustomImageFolder(DatasetFolder):
+    def __init__(self, root, file_path, transform=None, target_transform=None, loader=default_loader):
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+        self.samples = []
+
+        with open(file_path, 'r') as f:
+            for line in f.readlines():
+                img_path, label = line.strip().split()
+                self.samples.append((os.path.join(root, img_path), int(label)))
+
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target
+
+    def __len__(self):
+        return len(self.samples)
+    
