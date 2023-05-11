e3faae9cdc271312fb9268c81f56aba54c00dffc
Sanket Gupte <6501744+sanketx@users.noreply.github.com>
2023-05-10 17:04:12 -0700

    added probcover init to the registry

--- ALFM/src/init_strategies/registry.py
+++ ALFM/src/init_strategies/registry.py
@@ -3,6 +3,7 @@
 from enum import Enum
 
 from ALFM.src.init_strategies.centroid_init import CentroidInit
+from ALFM.src.init_strategies.probcover_init import ProbcoverInit
 from ALFM.src.init_strategies.random_init import RandomInit
 from ALFM.src.init_strategies.typiclust_init import TypiclustInit
 
@@ -13,3 +14,4 @@ class InitType(Enum):
     random_init = RandomInit
     typiclust_init = TypiclustInit
     centroid_init = CentroidInit
+    probcover_init = ProbcoverInit
